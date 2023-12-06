e87984adeabdfbb937660f854b5401a6b404587b
szydob <117864567+szydob@users.noreply.github.com>
2023-12-06 12:42:06 +0100

    update

---
+++ cmake-build-debug/googletest-master/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/szydo/Desktop/NetSim/googletest-master/googlemock
+# Build directory: C:/Users/szydo/Desktop/NetSim/cmake-build-debug/googletest-master/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
